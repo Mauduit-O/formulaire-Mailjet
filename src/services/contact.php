70de7c547f31f0fc9dc20581a6daa9e93a924c45
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2021-09-29 18:51:12 +0200

    add users choices condition

--- src/services/contact.php
+++ src/services/contact.php
@@ -7,6 +7,113 @@ require '../vendor/autoload.php';
     $token = $_GET['token'];  
     $_SESSION['user'] = $data['token']; 
 
+    //  Cette fonction défini le prix en fonction du choix selectioné
+    $priceFunction = function ()
+    {
+        $propertyType = htmlspecialchars($_POST['property-type']);
+        $year = htmlspecialchars($_POST['year']);
+        $priceTypeAVeryOld = '299';
+        $priceTypeBVeryOld = '249';
+        $priceTypeAOld = '199';
+        $priceTypeBOld = '149';
+        $priceTypeARecent = '99';
+        $priceTypeBRecent = '49';
+
+        $yearVeryOld = $year=="Avant 1949";
+        $yearOld = $year=="1949 - 1997";
+        $yearRecent = $year=="Apres 1997";
+
+        $propertyTypeA = $propertyType=="Bien de type A";
+        $propertyTypeB = $propertyType=="Bien de type B";
+
+
+        if ($yearVeryOld & $propertyTypeA)
+        {
+            return ($priceTypeAVeryOld);
+        } 
+        elseif ($yearVeryOld & $propertyTypeB)
+        {
+            return ($priceTypeBVeryOld);
+        } 
+        elseif ($yearOld & $propertyTypeA)
+        {
+            return ($priceTypeAOld);
+        }  
+        elseif ($yearOld & $propertyTypeB)
+        {
+            return ($priceTypeBOld);
+        }
+        elseif ($yearRecent & $propertyTypeA)
+        {
+            return ($priceTypeARecent);
+        
+        } else
+        {
+            return ($priceTypeBRecent);
+        }
+    };
+    echo  $price = $priceFunction();
+
+
+    //  Cette fonction défini le diagnostic à effectuer en fonction du choix selectioné
+    $diagnosticFunction = function ()
+    {
+        $propertyType = htmlspecialchars($_POST['property-type']);
+        $year = htmlspecialchars($_POST['year']);
+
+        $yearVeryOld = $year=="Avant 1949";
+        $yearOld = $year=="1949 - 1997";
+        $yearRecent = $year=="Apres 1997";
+
+        
+        $diagnosticVeryOld = 'DPE, AM, TER, PB';
+        $diagnosticOld = 'DPE, AM, TER';
+        $diagbosticOldRecent = 'DPE, TER';
+
+        if ($yearVeryOld )
+        {
+            return ($diagnosticVeryOld);
+        
+        } elseif ($yearOld) {
+
+            return ($diagnosticOld);
+
+        }else {
+            return ($diagbosticOldRecent);
+        } 
+        
+    };
+    echo $diagnostic = $diagnosticFunction();
+
+
+
+    //  Cette fonction défini la durée du diagnostic en fonction du choix selectioné
+    $timeFunction = function ()
+    {
+        $year = htmlspecialchars($_POST['year']);
+        $veryOld= $year=="Avant 1949";
+        $old= $year=="1949 - 1997";
+        $recent = $year=="Apres 1997";
+
+        $timeVeryOld = '1 h 30';
+        $timeOld = '1 h 00';
+        $timeVeryRecent = '00 h 30';
+
+
+        if ($veryOld)
+        {
+            return ($timeVeryOld);
+        
+        } elseif ($old) {
+
+            return ( $timeOld);
+        }else {
+            return ($timeVeryRecent);
+        } 
+        
+    };  
+    echo $time = $timeFunction();
+
 
     // Si les variables existent et qu'elles ne sont pas vides
     if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['address']) && !empty($_POST['code']) && !empty($_POST['city']) && !empty($_POST['property-type']) && !empty($_POST['year']) ){
@@ -43,20 +150,20 @@ require '../vendor/autoload.php';
                 ],
                 'Subject' => "Nouveau lead",
                 'TextPart' => 
-                "Nouveau lead ! 
+                "Nouveau lead !
 
                 Source prospect : $pseudo   
 
                 Son client 
                 Nom : $surname    
                 Prénom : $firstname    
-                Adresse : $address, $code, $city    
+                Adresse : $address, $code, $city   
                 Téléphone : $phone    
                 Email : $email    
                 Type de bien : $propertyType    
-                Année de construction : $year    
-                Offre : 299 €", 
-            ]
+                Année de construction : $year
+                Offre : $price €", 
+                ]
             ]
         ];
         $response = $mj->post(Resources::$Email, ['body' => $body]);
@@ -74,13 +181,13 @@ require '../vendor/autoload.php';
                 'Name' => "Dimo"
             ]
                 ],
-                'Subject' => "Nouveau lead",
+                'Subject' => "Demande bien reçu",
                 'TextPart' => 
                 "Nous avons bien reçu votre demande. Veuillez trouver le récapitulatif ci dessous : 
                 
                 Source prospect : $pseudo   
 
-                Votre client
+                Votre client 
                 Nom : $surname    
                 Prénom : $firstname    
                 Adresse : $address, $code, $city    
@@ -88,8 +195,9 @@ require '../vendor/autoload.php';
                 Email : $email    
                 Type de bien : $propertyType    
                 Année de construction : $year
-                Diag à réaliser : DPE, AM, TER, PB  
-                Offre : 299 €", 
+                Diag à réaliser : $diagnostic 
+                Durée: $time
+                Offre : $price €", 
             ]
             ]
         ];
@@ -115,8 +223,9 @@ require '../vendor/autoload.php';
                 $pseudo nous a sollicité afin vous adresser une offre dans le cadre de votre projet.
                 
                 Pour votre projet immobilier pour le bien situé à cette adresse : $address, $code, $city
-                Il vous faudra réaliser : DPE, AM, TER, PB 
-                Notre offre pour cette prestation : 299 € TTC
+                Il vous faudra réaliser : $diagnostic pour une durée de $time
+                Notre offre pour cette prestation : $price € TTC
+                
                 
                 Nous restons à votre disposition pour des informations complémentaires. 
                 
@@ -137,7 +246,6 @@ require '../vendor/autoload.php';
     }else {
         header('Location:index.php');
     }    
-    // $chemin = "user.php?token=".$data['token']."?reg_err=form";
     header("Location: user.php?token=".$data['token']."&reg_err=form");
     die();
 ?>
